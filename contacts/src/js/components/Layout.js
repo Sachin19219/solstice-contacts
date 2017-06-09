09c2290d05d10c5917bddbe134a276345faf88b9
Patrick Wilson <pat@cribspot.com>
2017-06-09 12:15:44 -0400

    updated required libs

--- contacts/src/js/components/Layout.js
+++ contacts/src/js/components/Layout.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Card from "./Card";
 import Detail from "./Detail";
-import Footer from "./Footer";
 import axios from 'axios';
 
 export default class Layout extends React.Component {
